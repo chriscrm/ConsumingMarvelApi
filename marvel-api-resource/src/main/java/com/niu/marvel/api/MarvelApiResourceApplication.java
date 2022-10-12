cd433eb98ae6a5513cfe4d494856e883a8b8f5cf
crisr-mac <cristian.monterrosa@gmail.com>
2022-10-12 17:54:41 -0600

    Base Initial Commit
    Dependencies added:
    -Spring Web
    -Spring DataJPA
    -Spring Validation
    -Spring DevTools
    -H2 Driver

---
+++ marvel-api-resource/src/main/java/com/niu/marvel/api/MarvelApiResourceApplication.java
@@ -0,0 +1,13 @@
+package com.niu.marvel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiResourceApplication.class, args);
+	}
+
+}
---
+++ marvel-api-resource/src/test/java/com/niu/marvel/api/MarvelApiResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niu.marvel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
